e40d0a0586ed709dcd47dc9960b10dbe61d982bf
Alfonso Marzano <marzanoalfonso@gmail.com>
2018-03-08 18:55:41 +0100

    First Commit

---
+++ Assets/Easy Unity Effect Editor/Editor/csParticleChanger.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class csParticleChanger : MonoBehaviour {
+
+    public void ObjectScaleChagne(float Value)
+    {
+        transform.localScale *= Value; // Change Object Scale Value.
+    }
+
+	public void ShurikenParticleScaleChange(float _Value)
+	{
+		//Define All of the Particle Object that included this Object
+		ParticleSystem[] ParticleSystems = GetComponentsInChildren<ParticleSystem>();
+
+		//Find Object One by One
+		foreach(ParticleSystem _ParticleSystem in ParticleSystems)
+        {
+			//Able to Approach not use SerializedObject
+			_ParticleSystem.startSpeed *= _Value;
+			_ParticleSystem.startSize *= _Value;
+			_ParticleSystem.gravityModifier *= _Value;
+			//Only SerializedObject can approach to the Selection Tap.
+			//So Define SerializedObject.
+			SerializedObject _SerializedObject = new SerializedObject(_ParticleSystem);
+			//Change Values
+			//if you want to change other value, refer "SerializedObject date of Shuriken Particle System.txt", write code.
+			_SerializedObject.FindProperty("CollisionModule.particleRadius").floatValue *= _Value;
+			_SerializedObject.FindProperty("ShapeModule.radius").floatValue *= _Value;
+			_SerializedObject.FindProperty("ShapeModule.boxX").floatValue *= _Value;
+			_SerializedObject.FindProperty("ShapeModule.boxY").floatValue *= _Value;
+			_SerializedObject.FindProperty("ShapeModule.boxZ").floatValue *= _Value;
+			_SerializedObject.FindProperty("VelocityModule.x.scalar").floatValue *= _Value;
+			_SerializedObject.FindProperty("VelocityModule.y.scalar").floatValue *= _Value;
+			_SerializedObject.FindProperty("VelocityModule.z.scalar").floatValue *= _Value;
+			_SerializedObject.FindProperty("ClampVelocityModule.x.scalar").floatValue *= _Value;
+			_SerializedObject.FindProperty("ClampVelocityModule.y.scalar").floatValue *= _Value;
+			_SerializedObject.FindProperty("ClampVelocityModule.z.scalar").floatValue *= _Value;
+			_SerializedObject.FindProperty("ClampVelocityModule.magnitude.scalar").floatValue *= _Value;
+			//Apply Values
+			_SerializedObject.ApplyModifiedProperties();
+		}
+	}
+
+    public void ShurikenParticleColorChange(Color ShurikenColor)
+    {
+        //Define All of the Particle Object that included this Object
+        ParticleSystem[] ParticleSystems = GetComponentsInChildren<ParticleSystem>();
+        //Find Object One by One
+        foreach (ParticleSystem _ParticleSystem in ParticleSystems)
+        {
+            //Change Color
+            _ParticleSystem.startColor = ShurikenColor;
+        }
+    }
+
+    public void LegacyEffectScaleChange(float Value)
+    {
+        //Define All of the Particle Object that included this Objects
+        ParticleEmitter[] ParticleEmitters = GetComponentsInChildren<ParticleEmitter>();
+        ParticleAnimator[] ParticleAnimators = GetComponentsInChildren<ParticleAnimator>();
+        ParticleRenderer[] ParticleRenderers = GetComponentsInChildren<ParticleRenderer>();
+
+        //Find Object One by One
+        foreach (ParticleEmitter _ParticleEmitter in ParticleEmitters)
+        {
+            //Change Value
+            _ParticleEmitter.minSize *= Value;
+            _ParticleEmitter.maxSize *= Value;
+            _ParticleEmitter.localVelocity *= Value;
+            _ParticleEmitter.rndAngularVelocity *= Value;
+            _ParticleEmitter.rndVelocity *= Value;
+            //Ellipsoid and tangetnVelocity is not able to approach normally.
+            //so use SerializedObject.
+            SerializedObject _SerializedObject = new SerializedObject(_ParticleEmitter);
+            _SerializedObject.FindProperty("m_Ellipsoid").vector3Value *= Value;
+            _SerializedObject.FindProperty("tangentVelocity").vector3Value *= Value;
+            _SerializedObject.ApplyModifiedProperties();
+        }
+
+        //Find Object One by One
+        foreach (ParticleAnimator _ParticleAnimator in ParticleAnimators)
+        {
+            //Change Value
+            _ParticleAnimator.sizeGrow *= Value;
+            _ParticleAnimator.force *= Value;
+            _ParticleAnimator.rndForce *= Value;
+        }
+
+        //Find Object One by One
+        foreach (ParticleRenderer _ParticleRenderer in ParticleRenderers)
+        {
+            //Change Value
+            _ParticleRenderer.maxParticleSize *= Value;
+        }
+    }
+}
---
+++ Assets/Easy Unity Effect Editor/Editor/csParticleEditor.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+
+[System.Serializable]
+
+public class csParticleEditor : EditorWindow {
+
+    private float Scale = 1;
+
+    public GameObject Effect;
+    public Color ShurikenSystemColor = Color.white;
+
+    static csParticleEditor myWindow;
+
+    static bool InitialButtonsPageOn; 
+    static bool EffectChangePageOn; 
+    static bool LegacyPageOn;
+    static bool ShurikenPageOn;
+    static bool LegacyShurikenPageOn;
+
+	[MenuItem("Particle Effect Editor/Particle Effect Editor")]
+
+	public static void Init()
+	{
+        myWindow = EditorWindow.GetWindowWithRect<csParticleEditor>(new Rect(100, 100, 300, 220)); //set Editor Position and Size
+		myWindow.title = "Particle Effect Editor"; //set Editor name
+
+        //initially, Display Legacy, Shuriken, Legacy&Shuriken Effect Change Section
+        //so InitialButtonPageOn set true.
+        InitialButtonsPageOn = true; 
+        //Other Page Hide.
+        EffectChangePageOn = false;
+        LegacyPageOn = false;
+        ShurikenPageOn = false;
+        LegacyShurikenPageOn = false;
+	}
+
+	void OnGUI()
+	{
+        if (InitialButtonsPageOn)
+        {
+            //Display Legacy Section Button
+            if (GUILayout.Button("Legacy Particle Effect Edit",  GUILayout.Height(70)))
+            {
+                InitialButtonsPageOn = false; //Hide Legacy, Shuriken, Legacy&Shuriken Effect Change Section
+
+                //Display Legacy Effect Section.
+                EffectChangePageOn = true; 
+                LegacyPageOn = true; 
+            }
+
+            //Display Shuriken Section Button
+            if (GUILayout.Button("Shuriken System Effect Edit",  GUILayout.Height(70)))
+            {
+                InitialButtonsPageOn = false; //Hide Legacy, Shuriken, Legacy&Shuriken Effect Change Section
+
+                //Display Shuriken Effect Effect Section.
+                EffectChangePageOn = true;
+                ShurikenPageOn = true; 
+            }
+
+            //Display Legacy & Shuriken Section Button
+            if (GUILayout.Button("Legacy & Shuriken Effect Edit",  GUILayout.Height(70)))
+            {
+                InitialButtonsPageOn = false; //Hide Legacy, Shuriken, Legacy&Shuriken Effect Change Section
+
+                //Display Legacy Effect Section.
+                EffectChangePageOn = true;
+                LegacyShurikenPageOn = true;
+            }
+        }
+
+        if (EffectChangePageOn == true)
+        {
+            if (LegacyPageOn)
+            {
+                GUILayout.Box("Legacy Effect Section", GUILayout.Width(295)); //Set Title Name
+                EditorGUILayout.Space();
+
+                Effect = (GameObject)EditorGUILayout.ObjectField("Legacy Effect", Effect, typeof(GameObject), true);  //For Change Object What you Want, Make ObjectField.
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+
+                Scale = float.Parse(EditorGUILayout.TextField("Scale Change Value", Scale.ToString())); // Make Scale Change TextFild
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+
+                if (GUILayout.Button("Scale Apply", GUILayout.Height(70))) // Make Scale Apply Button
+                {
+                    if (Effect.GetComponent<csParticleChanger>() != null) //Check Object has csParticleChanger Component
+                        Effect.GetComponent<csParticleChanger>().LegacyEffectScaleChange(Scale); //if Object has csParticleChagner, Just Change Value
+                    else
+                    {
+                        Effect.AddComponent<csParticleChanger>();//if Object hasn't csParticle Chagner, After Put in csParticleChanger, Change Value
+                        Effect.GetComponent<csParticleChanger>().LegacyEffectScaleChange(Scale);
+                    }
+                    DestroyImmediate(Effect.GetComponent<csParticleChanger>()); //After all work done, Destroy csParticleChanger
+                }
+            }
+
+            else if (ShurikenPageOn)
+            {
+                GUILayout.Box("Shuriken Effect Section", GUILayout.Width(295)); //Set Title Name
+                EditorGUILayout.Space();
+
+                Effect = (GameObject)EditorGUILayout.ObjectField("Shuriken Effect", Effect, typeof(GameObject), true); //For Change Object What you Want, Make ObjectField.
+                EditorGUILayout.Space();
+
+                ShurikenSystemColor = EditorGUILayout.ColorField("Shuriken Effect Color", ShurikenSystemColor); //For Change Effect's Color, Make ColorFiled 
+                EditorGUILayout.Space();
+
+                if (GUILayout.Button("Color Apply", GUILayout.Height(35))) //Make Color Apply Button
+                {
+                    if (Effect.GetComponent<csParticleChanger>() != null) //Check Object has csParticleChanger Component
+                    {
+                        Effect.GetComponent<csParticleChanger>().ShurikenParticleColorChange(ShurikenSystemColor); //if Object has csParticleChagner, Just Change Color
+                        Effect.GetComponent<csParticleChanger>().ObjectScaleChagne(Scale);
+                    }
+                    else
+                    {
+                        Effect.AddComponent<csParticleChanger>();//if Object hasn't csParticle Chagner, After Put in csParticleChanger, Change Color
+                        Effect.GetComponent<csParticleChanger>().ShurikenParticleColorChange(ShurikenSystemColor);
+                    }
+                    DestroyImmediate(Effect.GetComponent<csParticleChanger>()); //After all work done, Destroy csParticleChanger
+                }
+                EditorGUILayout.Space();
+
+                Scale = float.Parse(EditorGUILayout.TextField("Scale Change Value", Scale.ToString()));  // Make Scale Change TextFild
+                EditorGUILayout.Space();
+
+                if (GUILayout.Button("Scale Apply", GUILayout.Height(35))) // Make Scale Apply Button
+                {
+                    if (Effect.GetComponent<csParticleChanger>() != null) //Check Object has csParticleChanger Component
+                    {
+                        Effect.GetComponent<csParticleChanger>().ShurikenParticleScaleChange(Scale); // if Object has csParticleChagner, Just Change Value
+                        Effect.GetComponent<csParticleChanger>().ObjectScaleChagne(Scale);
+                    }
+                    else
+                    {
+                        Effect.AddComponent<csParticleChanger>();//if Object hasn't csParticle Chagner, After Put in csParticleChanger, Change Value
+                        Effect.GetComponent<csParticleChanger>().ShurikenParticleScaleChange(Scale);
+                    }
+                    DestroyImmediate(Effect.GetComponent<csParticleChanger>()); //After all work done, Destroy csParticleChanger
+                }
+            }
+
+            else if (LegacyShurikenPageOn)
+            {
+                GUILayout.Box("Legacy&Shuriken Effect Section", GUILayout.Width(295));// Set Title Name
+                EditorGUILayout.Space();
+
+                Effect = (GameObject)EditorGUILayout.ObjectField("Legacy&Shuriken Effect", Effect, typeof(GameObject), true); //For Change Object What you Want, Make ObjectField.
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+
+                Scale = float.Parse(EditorGUILayout.TextField("Scale Change Value", Scale.ToString())); // Make Scale Change TextFild
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+
+                if (GUILayout.Button("Scale Apply", GUILayout.Height(70))) // Make Scale Apply Button
+                {
+                    if (Effect.GetComponent<csParticleChanger>() != null)//Check Object has csParticleChanger Component
+                    {
+                        Effect.GetComponent<csParticleChanger>().LegacyEffectScaleChange(Scale); // if Object has csParticleChagner, Just Change Value
+                        Effect.GetComponent<csParticleChanger>().ShurikenParticleScaleChange(Scale);
+                        Effect.GetComponent<csParticleChanger>().ObjectScaleChagne(Scale);
+                    }
+                    else
+                    {
+                        Effect.AddComponent<csParticleChanger>(); //if Object hasn't csParticle Chagner, After Put in csParticleChanger, Change Value
+                        Effect.GetComponent<csParticleChanger>().LegacyEffectScaleChange(Scale);
+                        Effect.GetComponent<csParticleChanger>().ShurikenParticleScaleChange(Scale);
+
+                    }
+                    DestroyImmediate(Effect.GetComponent<csParticleChanger>()); //After all work done, Destroy csParticleChanger
+                }
+            }
+            if (GUILayout.Button("Back", GUILayout.Height(20),GUILayout.Width(40))) //Make Back Button
+            {
+                InitialButtonsPageOn = true;
+                EffectChangePageOn = false;
+                LegacyPageOn = false;
+                ShurikenPageOn = false;
+                LegacyShurikenPageOn = false;
+            }
+        }
+	}
+}
+#endif
